2ba3a77b89d178b6d10bb63e49adfcae197cefa3
Wissam <wissamjemal99@gmail.com>
2024-11-23 12:18:11 +0300

    working on frontend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="p-3 flex-col space-y-2 items-center justify-center">
+      <div className="mockup-window bg-base-300 border flex">
+        <div>
+          {/* User info div */}
+          <div className="flex space-x-2 bg-white h-36 w-1/4">
+            <div className="avatar placeholder p-2 h-20">
+              <div className="bg-neutral text-neutral-content w-8 rounded-full">
+                <span className="text-xl">AB</span>
+              </div>
+            </div>
+          </div>
+
+          {/* blog Header and body div */}
+          <div></div>
+        </div>
+      </div>
+
+      <div className="mockup-window bg-base-300 border"></div>
+    </div>
+  );
 };
 
 export default HomePage;
