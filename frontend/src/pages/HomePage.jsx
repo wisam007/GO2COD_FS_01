b275fee26d95af620f3da7825dd24bca7540b7b2
Wissam <wissamjemal99@gmail.com>
2024-11-27 01:27:42 +0300

    resolving merge conflicts

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,38 +1,11 @@
-<<<<<<< HEAD
 import { BeatLoader } from "react-spinners";
 import BlogCard from "../components/BlogCard";
 import Heading from "../components/Heading";
 import React, { useContext } from "react";
 import { BlogContext } from "../context/BlogContext";
-import { Link } from "react-router-dom";
 const HomePage = () => {
   const { isLoading, blogs } = useContext(BlogContext);
 
-=======
-import React from "react";
-import { BeatLoader } from "react-spinners";
-import { useEffect, useState } from "react";
-import BlogCard from "../components/BlogCard";
-import Heading from "../components/Heading";
-import axios from "axios";
-const HomePage = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [blogs, setBlogs] = useState([]);
-  useEffect(() => {
-    const fetchBlogs = async () => {
-      try {
-        setIsLoading(true);
-        const response = await axios.get("http://localhost:8000/api/blogs/");
-        setBlogs(response.data);
-        console.log(response.data);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchBlogs();
-  }, []);
->>>>>>> 2472349f217b5e4588a5673a926f9f8f87454dd2
   return (
     <>
       <Heading title="Recent Blogs" />
